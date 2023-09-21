cfb6219234c3b0cc07174db74e5f3a57ad93be43
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-09-20 23:58:51 -0300

    enviando arquivos da pasta domain.model e application

---
+++ src/test/java/me/dio/santanderdevweb2023/SantanderDevWeb2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweb2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeb2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
