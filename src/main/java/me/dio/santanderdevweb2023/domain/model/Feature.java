f68dfd575d8ee613cb911e673bd05273378abbe5
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-09-21 02:32:52 -0300

    trabalhando a camada de dominio e bd

---
+++ src/main/java/me/dio/santanderdevweb2023/domain/model/Feature.java
@@ -0,0 +1,12 @@
+package me.dio.santanderdevweb2023.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+
+}
---
+++ src/main/java/me/dio/santanderdevweb2023/domain/model/News.java
@@ -0,0 +1,10 @@
+package me.dio.santanderdevweb2023.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
